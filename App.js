b61f91e5dc9c54278740aa5df9b54c1c01ae2260
Mamadou Moustapha Diallo <moustaphaotf@gmail.com>
2023-01-13 17:03:45 +0000

    Setting up the app to use components and functionnalities developped

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
+import Game from './screens/Game';
+import Home from './screens/Home';
+import { useFonts } from 'expo-font'
 import { StyleSheet, Text, View } from 'react-native';
+import { colors } from './global';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    "ds-digital": require('./assets/ds-digital.ttf')
+  });
+
+  if(!fontLoaded){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Welcome on Clockzzle</Text>
+        <Text style={styles.text}>We help you learn telling time</Text>
+        <Text style={styles.text}>Enjoy the app</Text>
+      </View>
+    );
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{}}
+      >
+        <Stack.Screen name="Home" component={Home} options={{headerShown:false}}/>
+        <Stack.Screen name="Game" component={Game} options={{title: "Clockzzle"}}/>
+      </Stack.Navigator>
+      <StatusBar style="auto"/>
+    </NavigationContainer>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    backgroundColor: colors.main,
+    flex: 1,
+  }, 
+  text: {
+    fontSize: 24,
+    fontWeight: '400',
+    color: '#284d70',
+    fontStyle: 'italic'
+  }
+})
---
+++ global.js
@@ -0,0 +1,7 @@
+export const colors = {
+  main: "#cbe5f4",
+  black: "#1A1A1AED",
+  gray: "#eeeeeeed",
+  danger: "#D9534f",
+  success: "green",
+}
